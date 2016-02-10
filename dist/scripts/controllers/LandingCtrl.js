e00b997a3cfb1d71883e93d865ac265877a5df00
Ben Rossi <ben@rossinet.com>
2016-02-10 13:59:35 -0500

    Checkpoint 5: Collection and Landing controllers.

---
+++ dist/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+  var LandingCtrl = function LandingCtrl() {
+    this.heroTitle = "Turn the Music Up!";
+  };
+  
+  angular
+    .module('blocJams')
+    .controller('LandingCtrl', LandingCtrl);
+})();
